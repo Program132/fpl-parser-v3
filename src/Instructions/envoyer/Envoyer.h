656751b88171fc2c1887469d551762b5694adb10
Program132 <clement.gauthier132@orange.fr>
2023-07-04 01:19:26 +0200

    Moved envoyer funcs

--- src/Instructions/envoyer/Envoyer.h
+++ src/Instructions/envoyer/Envoyer.h
@@ -3,9 +3,9 @@
 #include <iostream>
 #include <vector>
 
-#include "../Essentials/Tokenizer/Tokenizer.h"
-#include "../Essentials/Data/Data.h"
-#include "../Essentials/Expecter/Expecter.h"
+#include "../../Essentials/Tokenizer/Tokenizer.h"
+#include "../../Essentials/Data/Data.h"
+#include "../../Essentials/Expecter/Expecter.h"
 
 namespace FPL::Instruction::Envoyer {
     void getInformation(std::vector<FPL::Essential::Tokenizer::Token>::iterator &currentToken, FPL::Essential::Data::Data &data, bool &pass);
