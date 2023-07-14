e677b88c4011c0700982631b90aff11da6004a53
Program132 <clement.gauthier132@orange.fr>
2023-07-14 20:52:09 +0200

    Chnaged variables classs

--- src/Instructions/VariablesUtils/Variables.cpp
+++ src/Instructions/VariablesUtils/Variables.cpp
@@ -1,7 +1,7 @@
 #include "Variables.h"
 
 namespace FPL::Instruction::VariablesUtils {
-    Variable defineVariable(std::vector<FPL::Essential::Tokenizer::Token>::iterator &currentToken, FPL::Essential::Data::Data &data) {
+    Variable defineVariable(std::vector<FPL::Essential::Tokenizer::Token>::iterator &currentToken, FPL::Essential::Data::Data &data, std::optional<Fonctions::Fonction> fonction) {
         auto var_type = ExpectType(currentToken);
         if (!var_type.has_value()) {
             forgotType(currentToken);
@@ -19,25 +19,40 @@ namespace FPL::Instruction::VariablesUtils {
         auto var_value = ExpectValue(currentToken);
         if (!var_value.has_value()) {
             auto possibleVar = ExpectIdentifiant(currentToken);
-            if (!possibleVar.has_value() && data.variableExist(possibleVar->content)) {
+            if (!possibleVar.has_value()) {
                 forgotValue(currentToken);
             }
 
-            Variable old_var = data.getVariable(possibleVar->content).value();
-            Variable var(var_name->content, old_var.getValue(), var_type.value());
+            if (data.variableExist(possibleVar->content)) {
+                Variable old_var = data.getVariable(possibleVar->content).value();
+                Variable var(var_name->content, old_var.getValue(), var_type.value());
 
-            if (old_var.getType() != var.getType()) {
-                differentTypes(currentToken);
+                if (old_var.getType() != var.getType()) {
+                    differentTypes(currentToken);
+                }
+
+                return var;
+            } else if (fonction.has_value() && fonction->isArgument(possibleVar->content)) {
+                Variable old_var(
+                        fonction->getArgument(possibleVar->content)->getName(),
+                        fonction->getArgument(possibleVar->content)->getValue().content,
+                        fonction->getArgument(possibleVar->content)->getType()
+                        );
+                Variable var(var_name->content, old_var.getValue(), var_type.value());
+
+                if (old_var.getType() != var.getType()) {
+                    differentTypes(currentToken);
+                }
+
+                return var;
             }
-
-            return var;
         } else {
             Variable var(var_name->content, var_value->content, var_type.value());
             return var;
         }
     }
 
-    Variable defineVariable_Paquet(std::vector<FPL::Essential::Tokenizer::Token>::iterator &currentToken, FPL::Essential::Data::Data &data, std::string paquetName) {
+    Variable defineVariable_Paquet(std::vector<FPL::Essential::Tokenizer::Token>::iterator &currentToken, FPL::Essential::Data::Data &data, std::string paquetName, std::optional<Fonctions::Fonction> fonction) {
         auto var_type = ExpectType(currentToken);
         if (!var_type.has_value()) {
             forgotType(currentToken);
--- src/Instructions/VariablesUtils/Variables.h
+++ src/Instructions/VariablesUtils/Variables.h
@@ -10,7 +10,11 @@
 #include "../../Essentials/ErrorsManager/ErrorsManager.h"
 #include "../../Utils/stringTo.h"
 
+#include "../../Essentials/Definitions/Fonctions/Fonctions.h"
+
+using namespace FPL::Definition;
+
 namespace FPL::Instruction::VariablesUtils {
-    Variable defineVariable(std::vector<FPL::Essential::Tokenizer::Token>::iterator &currentToken, FPL::Essential::Data::Data &data);
-    Variable defineVariable_Paquet(std::vector<FPL::Essential::Tokenizer::Token>::iterator &currentToken, FPL::Essential::Data::Data &data, std::string paquetName);
+    Variable defineVariable(std::vector<FPL::Essential::Tokenizer::Token>::iterator &currentToken, FPL::Essential::Data::Data &data, std::optional<Fonctions::Fonction> fonction);
+    Variable defineVariable_Paquet(std::vector<FPL::Essential::Tokenizer::Token>::iterator &currentToken, FPL::Essential::Data::Data &data, std::string paquetName, std::optional<Fonctions::Fonction> fonction);
 }
